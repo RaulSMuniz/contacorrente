45a0a64db38ac57e0c1101d9f9e567e9c511465b
Raul Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2023-08-28 15:09:15 -0300

    ContaCorrente2.java
    adicionados comentários sobre o código

--- src/main/java/com/mycompany/conta/corrente/ContaCorrente2.java
+++ src/main/java/com/mycompany/conta/corrente/ContaCorrente2.java
@@ -1,12 +1,16 @@
 package com.mycompany.conta.corrente;
 
 public class ContaCorrente2 {
+    // variáveis necessárias para a operação
     public double deposito, saque, saldo, taxado;
+
+    // operação de saque e taxa
     public double operacao(){
         saldo = deposito - saque;
         taxado = saldo - saldo * 0.005;
         return taxado;
     }
+    // valor do saldo após o saque (já taxado)
     public void valor(){
         operacao();
         System.out.println("Seu saldo após o saque é: "+ taxado);
