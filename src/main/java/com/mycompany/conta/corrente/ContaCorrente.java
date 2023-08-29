6b7a9b8d98134fcfa9c5dbcf41d4abff143266a0
Raul Muniz <136410938+RaulSMuniz@users.noreply.github.com>
2023-08-28 22:04:43 -0300

    ContaCorrente.java
    adicionados scanners

--- src/main/java/com/mycompany/conta/corrente/ContaCorrente.java
+++ src/main/java/com/mycompany/conta/corrente/ContaCorrente.java
@@ -1,11 +1,18 @@
 package com.mycompany.conta.corrente;
-
+import java.util.Scanner;
 public class ContaCorrente {
 
     public static void main(String[] args) {
         ContaCorrente2 cal = new ContaCorrente2();
-        cal.deposito = 500;
-        cal.saque = 200;
+        Scanner sd = new Scanner(System.in);
+        System.out.println("Digite o valor que deseja depositar:");
+        cal.deposito = (sd.nextInt());
+        
+        Scanner ss = new Scanner(System.in);
+        System.out.println("Digite o valor que deseja sacar:");
+        cal.saque = (ss.nextInt());
         cal.valor();
+        sd.close();
+        ss.close();
     }
 }
